60e058c4f8359ff09e3d140363103a6e4eae55ea
Chetan-Goyal <harshrock616@gmail.com>
2021-12-06 01:28:41 +0530

    Abstraction

---
+++ abstraction-const.js
@@ -0,0 +1,12 @@
+// ! Private Data - DON'T SHARE
+const APIKey = 'DFGSJDFGKJW4T9345T9RGJ49FJA03'
+
+
+// Public Data
+const myName = "Chetan Goyal"
+
+const helloName = (val) => {
+  console.log(`Hello ${val}`)
+}
+
+module.exports = { myName, helloName }
---
+++ abstraction-implementation.js
@@ -0,0 +1,8 @@
+// importing module
+const data = require('./abstraction-const')
+
+data.helloName(data.myName)
+
+// * Not Working
+// const { copyOfName, copyOfHelloName } = require('./encap-const')
+// console.log(copyOfName)
