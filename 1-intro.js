35a152bdc9e3430bada12a1d0f1510a1ee120437
Chetan-Goyal <harshrock616@gmail.com>
2021-12-06 00:11:04 +0530

    Intro

---
+++ 1-intro.js
@@ -0,0 +1,8 @@
+const myName = "Chetan";
+
+console.log(myName);
+
+if (myName.length > 4)
+  console.log('name is longer than 4 characters')
+else
+  console.log('name is shorter than or equal to 4 characters')
