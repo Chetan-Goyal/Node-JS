1365cc64489310fe50184d5a2355facee1c3dbee
Chetan-Goyal <harshrock616@gmail.com>
2021-12-07 22:20:28 +0530

    Adds OS Module Usage

---
+++ os_module.js
@@ -0,0 +1,16 @@
+const os = require('os')
+
+console.log(os.version())
+
+console.log(os.userInfo())
+
+console.log(os.cpus())
+
+console.log(os.hostname())
+
+console.log(os.loadavg())
+
+console.log(os.networkInterfaces())
+
+console.log(os.uptime())
+
