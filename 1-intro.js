431dc7be2171e7dd92c49f484ead0cf13fa507d8
Chetan-Goyal <harshrock616@gmail.com>
2021-12-08 23:01:40 +0530

    string compare testing

--- 1-intro.js
+++ 1-intro.js
@@ -2,7 +2,8 @@ const myName = "Chetan";
 
 console.log(myName);
 
-if (myName.length > 4)
-  console.log('name is longer than 4 characters')
+
+if (myName == 'Chetan')
+  console.log('myName is Chetan')
 else
-  console.log('name is shorter than or equal to 4 characters')
+  console.log('myName is not Chetan')
